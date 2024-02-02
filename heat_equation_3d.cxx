b2dd6b2ef116ac1928f132c725172cc4f805cf64
fm16191 <56755856+fm16191@users.noreply.github.com>
2024-02-02 10:32:19 +0100

    3d : Add periodic boundary conditions

--- heat_equation_3d.cxx
+++ heat_equation_3d.cxx
@@ -10,16 +10,8 @@ double NB_Y = 100;
 double NB_Z = 100;
 double NB_T = 100;
 
-double DX = 1 / (NB_X + 1);
-double DY = 1 / (NB_Y + 1);
-double DZ = 1 / (NB_Z + 1);
-double D = 2.3 * 10e-5;
-
-double DT; // defined afterwards
-
-double DX2 = DX * DX;
-double DY2 = DY * DY;
-double DZ2 = DZ * DZ;
+double D = 2.3e-5;
+double DT = 0.001;
 
 int write_interval = 0;
 
@@ -57,7 +49,7 @@ void write_results(vector<vector<vector<double>>> u, size_t step)
     }
     out_file << "\n";
 
-    printf("Iteration %ld written to %s\n", step, out_filename.c_str());
+    printf("t=%ld written to %s\n", step, out_filename.c_str());
 }
 
 int print_usage(char *exec)
@@ -77,6 +69,27 @@ int print_usage(char *exec)
            " -h, --help Show this message and exit\n",
            NB_X, NB_Y, NB_Z, NB_T, D, out_filename.c_str());
     return 0;
+}
+
+double compute_total_temp(vector<vector<vector<double>>> u)
+{
+    double sum = 0.0;
+    for (size_t i = 1; i < NB_X + 1; ++i) {
+        for (size_t j = 1; j < NB_Y + 1; ++j) {
+            for (size_t k = 1; k < NB_Z + 1; ++k) {
+                sum += u[i][j][k];
+            }
+        }
+    }
+    return sum;
+}
+
+void ensure_mass_conservation(double initial_temp, vector<vector<vector<double>>> u, size_t t)
+{
+    double total_temp = compute_total_temp(u);
+    double mass_change = fabs(initial_temp - total_temp);
+    printf("t=%ld Total temperature : %.7e, change in mass: %.1e (should be close to 0)\n", t,
+           total_temp, mass_change);
 }
 
 int main(int argc, char *argv[])
@@ -133,8 +146,15 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    double DX = 1 / (NB_X + 1);
+    double DY = 1 / (NB_Y + 1);
+    double DZ = 1 / (NB_Z + 1);
+    double DX2 = DX * DX;
+    double DY2 = DY * DY;
+    double DZ2 = DZ * DZ;
+
     // Ensure stability
-    double DT = (DX2 * DY2) / (2 * D * (DX2 + DY2)) * 0.5;
+    // double DT = (DX2 * DY2) / (2 * D * (DX2 + DY2)) * 0.5;
 
     // Verify stability
     // α = D △t/△x^2, β = D △t/△y^2, α + β < 1/2
@@ -156,43 +176,43 @@ int main(int argc, char *argv[])
     cerr << "  Step in t (dt)                : " << DT << "\n";
 
     if (write_interval)
-        cerr << "  Write interval            : " << write_interval << "\n";
+        cerr << "  Write interval                : " << write_interval << "\n";
 
-    cerr << "  Stability indicator       : " << stability << "\n";
+    cerr << "  Stability indicator           : " << stability << "\n\n";
 
     vector<vector<vector<double>>> u(NB_X + 2,
                                      vector<vector<double>>(NB_Y + 2, vector<double>(NB_Z + 2, 0)));
 
-    // Boundary conditions
-    double ux0 = 40;
-    double uxa = 0;
-    double uy0 = 40;
-    double uya = 0;
-    double uz0 = 40;
-    double uza = 0;
+    // // Boundary conditions
+    // double ux0 = 40;
+    // double uxa = 0;
+    // double uy0 = 40;
+    // double uya = 0;
+    // double uz0 = 40;
+    // double uza = 0;
 
-    for (size_t j = 0; j < NB_Y + 2; ++j) {
-        for (size_t k = 0; k < NB_Y + 2; ++k) {
-            u[0][j][k] = ux0;
-            u[NB_X + 1][j][k] = uxa;
-        }
-    }
+    // for (size_t j = 0; j < NB_Y + 2; ++j) {
+    //     for (size_t k = 0; k < NB_Y + 2; ++k) {
+    //         u[0][j][k] = ux0;
+    //         u[NB_X + 1][j][k] = uxa;
+    //     }
+    // }
 
-    for (size_t i = 0; i < NB_X + 2; ++i) {
-        for (size_t k = 0; k < NB_Z + 2; ++k) {
-            u[i][0][k] = uy0;
-            u[i][NB_Y + 1][k] = uya;
-        }
-    }
+    // for (size_t i = 0; i < NB_X + 2; ++i) {
+    //     for (size_t k = 0; k < NB_Z + 2; ++k) {
+    //         u[i][0][k] = uy0;
+    //         u[i][NB_Y + 1][k] = uya;
+    //     }
+    // }
 
-    for (size_t i = 0; i < NB_X + 2; ++i) {
-        for (size_t j = 0; j < NB_Y + 2; ++j) {
-            u[i][j][0] = uz0;
-            u[i][j][NB_Z + 1] = uza;
-        }
-    }
+    // for (size_t i = 0; i < NB_X + 2; ++i) {
+    //     for (size_t j = 0; j < NB_Y + 2; ++j) {
+    //         u[i][j][0] = uz0;
+    //         u[i][j][NB_Z + 1] = uza;
+    //     }
+    // }
 
-    // Init condition : a 100 °C circle 2/3 the size of the simulation at its center
+    // Init condition : a 100 °C sphere 2/3 the size of the simulation at its center
     double center_x = (NB_X + 1) / 2.0;
     double center_y = (NB_Y + 1) / 2.0;
     double center_z = (NB_Z + 1) / 2.0;
@@ -213,12 +233,38 @@ int main(int argc, char *argv[])
 
     vector<vector<vector<double>>> u_next(u);
 
+    double initial_temp = compute_total_temp(u);
+    printf("Initial total temperature       : %.7e\n", initial_temp);
+
     // Iterate
     double a = D * DT / DX2;
     double b = D * DT / DY2;
     double c = D * DT / DZ2;
     double d = (1 - 2 * a - 2 * b - 2 * c);
     for (size_t t = 0; t < NB_T; ++t) {
+        // Copy from one plan to the other
+        // X axis
+        for (size_t j = 0; j < NB_Y + 2; ++j) {
+            for (size_t k = 0; k < NB_Z + 2; ++k) {
+                u[0][j][k] = u[NB_X][j][k];
+                u[NB_X + 1][j][k] = u[1][j][k];
+            }
+        }
+        // Y axis
+        for (size_t i = 0; i < NB_X + 2; ++i) {
+            for (size_t k = 0; k < NB_Z + 2; ++k) {
+                u[i][0][k] = u[i][NB_Y][k];
+                u[i][NB_Y + 1][k] = u[i][1][k];
+            }
+        }
+        // Z axis
+        for (size_t i = 0; i < NB_X + 2; ++i) {
+            for (size_t j = 0; j < NB_Y + 2; ++j) {
+                u[i][j][0] = u[i][j][NB_Z];
+                u[i][j][NB_Z + 1] = u[i][j][1];
+            }
+        }
+
         for (size_t i = 1; i < NB_X + 1; ++i) {
             for (size_t j = 1; j < NB_Y + 1; ++j) {
                 for (size_t k = 1; k < NB_Z + 1; ++k) {
@@ -229,10 +275,13 @@ int main(int argc, char *argv[])
             }
         }
         u.swap(u_next);
-        if (write_interval && t % write_interval == 0)
+        if (write_interval && t % write_interval == 0 && t != 0) {
+            ensure_mass_conservation(initial_temp, u, t);
             write_results(u, t);
+        }
     }
 
+    ensure_mass_conservation(initial_temp, u, (size_t)NB_T - 1);
     write_results(u, NB_T);
 
     return 0;
