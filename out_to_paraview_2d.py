6eb1560a8da248d24852e4a801569c20d7f674fe
fm16191 <56755856+fm16191@users.noreply.github.com>
2024-02-14 10:20:37 +0100

    Fix first iteration skipped, update outputs

--- out_to_paraview_2d.py
+++ out_to_paraview_2d.py
@@ -7,15 +7,11 @@ import shutil
 
 # Get data file
 if len(sys.argv) < 2:
-    print("Export output to CSV file that Paraview can read")
+    print("Export 2d output to CSV files that Paraview can read")
     print(f"Usage: {sys.argv[0]} <file.data>")
     exit(1)
 
 data_file = sys.argv[1]
-
-fo = open(data_file, "r")
-line = fo.readline()
-SIZE_X_AXIS, SIZE_Y_AXIS = map(int, line.split()[:2])
 
 # Create folder
 output_directory = f"csv_{data_file}"
@@ -26,6 +22,18 @@ if os.path.exists(output_directory):
 
 os.makedirs(output_directory)
 
+# Get simulation dimensions
+fo = open(data_file, "r")
+line = fo.readline()
+SIZE_X_AXIS, SIZE_Y_AXIS = map(int, line.split()[:2])
+lines = sum(1 for line in fo) + 1 # first line is already read
+print(f"Simulation dimensions are {SIZE_X_AXIS} x {SIZE_Y_AXIS}")
+print(f"Simulation has {lines} steps")
+fo.close()
+fo = open(data_file, "r")
+
+
+# Create CSV files
 for t, line in enumerate(fo.readlines()):
     data = np.array(list(map(float, line.split()[2:])))
 
@@ -42,3 +50,5 @@ for t, line in enumerate(fo.readlines()):
 
     fw.close()
 fo.close()
+
+print(f"CSV files written to \"{output_directory}/\"")
--- out_to_paraview_3d.py
+++ out_to_paraview_3d.py
@@ -7,15 +7,11 @@ import shutil
 
 # Get data file
 if len(sys.argv) < 2:
-    print("Export output to CSV file that Paraview can read")
+    print("Export 3d utput to CSV files that Paraview can read")
     print(f"Usage: {sys.argv[0]} <file.data>")
     exit(1)
 
 data_file = sys.argv[1]
-
-fo = open(data_file, "r")
-line = fo.readline()
-SIZE_X_AXIS, SIZE_Y_AXIS, SIZE_Z_AXIS = map(int, line.split()[:3])
 
 # Create folder
 output_directory = f"csv_{data_file}"
@@ -26,6 +22,18 @@ if os.path.exists(output_directory):
 
 os.makedirs(output_directory)
 
+# Get simulation dimensions
+fo = open(data_file, "r")
+line = fo.readline()
+SIZE_X_AXIS, SIZE_Y_AXIS, SIZE_Z_AXIS = map(int, line.split()[:3])
+lines = sum(1 for line in fo) + 1 # first line is already read
+print(f"Simulation dimensions are {SIZE_X_AXIS} x {SIZE_Y_AXIS} x {SIZE_Z_AXIS}")
+print(f"Simulation has {lines} steps")
+fo.close()
+fo = open(data_file, "r")
+
+
+# Create CSV files
 for t, line in enumerate(fo.readlines()):
     data = np.array(list(map(float, line.split()[3:])))
 
@@ -37,8 +45,9 @@ for t, line in enumerate(fo.readlines()):
     print(SIZE_X_AXIS, SIZE_Y_AXIS, SIZE_Z_AXIS, len(data), SIZE_X_AXIS * SIZE_Y_AXIS * SIZE_Z_AXIS)
 
     for i in range(len(data)):
-        # fw.write(f"{int(i / SIZE_Y_AXIS)}, {i % SIZE_Y_AXIS}, 0, {data[i]}\n")
         fw.write(f"{int(i / SIZE_Y_AXIS / SIZE_Z_AXIS)}, {int(i / SIZE_Z_AXIS) % SIZE_Y_AXIS}, {i % SIZE_Z_AXIS}, {data[i]}\n")
 
     fw.close()
 fo.close()
+
+print(f"CSV files written to \"{output_directory}/\"")
